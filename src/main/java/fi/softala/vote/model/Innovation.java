b53d1c9ad53e28c38332ce3ef6de61a26de38777
Jarmala Timo <a1500863@myy.haaga-helia.fi>
2016-09-13 10:55:45 +0300

    Innovation class with getters and setters

---
+++ src/main/java/fi/softala/vote/model/Innovation.java
@@ -0,0 +1,17 @@
+package fi.softala.vote.model;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+
+
+
+public class Innovation {
+	
+	
+@Getter @Setter int id;
+
+@Getter @Setter String name;
+@Getter @Setter String topic;
+	
+
+}
