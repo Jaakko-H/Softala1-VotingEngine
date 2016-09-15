c75d693970cce2226f06b1f336f3590f40f7dfcc
Heikelä Jaakko <a1400155@myy.haaga-helia.fi>
2016-09-15 10:00:21 +0300

    tidy code
    Format source code into a cleaner form. Remove unnecessary blank lines.

--- src/test/java/fi/softala/innovation/InnovationDaoJDBCSpringimplTest.java
+++ src/test/java/fi/softala/innovation/InnovationDaoJDBCSpringimplTest.java
@@ -7,28 +7,13 @@ import fi.softala.vote.dao.DaoInnovation;
 import fi.softala.vote.model.*;
 
 public class InnovationDaoJDBCSpringimplTest implements DaoInnovation{
-	
-	
-	
-public List<Innovation>	getInnovations(){
-	
-	
-	List <Innovation> innovations=new ArrayList<Innovation>();
-	Innovation innovation=new Innovation();
-	
-	innovation.id=1;
-	
-	innovation.name="Virtual glasses";
-	
-	innovation.topic="Virtuality";
-	
-	
-	
-	innovations.add(innovation);
-	
-	
-	
-	return innovations;
-}
-
+	public List<Innovation>	getInnovations(){
+		List <Innovation> innovations=new ArrayList<Innovation>();
+		Innovation innovation=new Innovation();
+		innovation.id=1;
+		innovation.name="Virtual glasses";
+		innovation.topic="Virtuality";
+		innovations.add(innovation);
+		return innovations;
+	}
 }
