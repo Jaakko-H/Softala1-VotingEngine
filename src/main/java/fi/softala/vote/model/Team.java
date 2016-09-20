27315d26d6c759ac96d3de0e4d44d84e267a6eeb
Heikelä Jaakko <a1400155@myy.haaga-helia.fi>
2016-09-20 11:44:30 +0300

    add team.java
    Add Team class.

---
+++ src/main/java/fi/softala/vote/model/Team.java
@@ -0,0 +1,9 @@
+package fi.softala.vote.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class Team {
+	@Getter @Setter private long teamID;
+	@Getter @Setter private String teamName;
+}
