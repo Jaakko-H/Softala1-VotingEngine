c00f00e870805e601c32888e8a9edb20783ead66
roope-kar <roope.karhapaa@outlook.com>
2016-09-20 09:58:22 +0300

    view skeleton, iterate over innovations

--- src/main/java/fi/softala/aani/MainCtrl.java
+++ src/main/java/fi/softala/aani/MainCtrl.java
@@ -1,14 +1,28 @@
 package fi.softala.aani;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import List;
+
+@Controller
 public class MainCtrl {
 
-    @RequestMapping("/")
-    public String index() {
-        return "Hello World";
+	@RequestMapping("/")
+    public String index(Model model) {
+		ArrayList<String> innovations = new ArrayList<>();
+		innovations.add("one");
+		innovations.add("two");
+		innovations.add("three");
+		
+		model.addAttribute("innovations", innovations);
+		
+        return "test";
     }
 
 }
