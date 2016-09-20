0a121601a1f5293ed9fdec5e7cb0b0df1f863dd8
roope-kar <roope.karhapaa@outlook.com>
2016-09-20 11:12:31 +0300

    innovation view, added bootstrap to build.gradle

--- src/main/java/fi/softala/aani/MainCtrl.java
+++ src/main/java/fi/softala/aani/MainCtrl.java
@@ -1,14 +1,11 @@
 package fi.softala.aani;
 
+
 import java.util.ArrayList;
-import java.util.Arrays;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import List;
 
 @Controller
 public class MainCtrl {
@@ -16,6 +13,7 @@ public class MainCtrl {
 	@RequestMapping("/")
     public String index(Model model) {
 		ArrayList<String> innovations = new ArrayList<>();
+		
 		innovations.add("one");
 		innovations.add("two");
 		innovations.add("three");
