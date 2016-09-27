deaef77d03ac658ff43e5dfef179469c16c2703b
Valleyaapo <aapohlaakso@gmail.com>
2016-09-27 12:06:57 +0300

    session invalidate

---
+++ src/main/java/fi/softala/vote/controller/VoteCtrl.java
@@ -0,0 +1,24 @@
+package fi.softala.vote.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import fi.softala.vote.model.Innovation;
+
+@Controller
+
+public class VoteCtrl {
+	@RequestMapping(path="/vote", method=RequestMethod.POST)
+	public String handleVote(@ModelAttribute() Innovation innovation, HttpSession session){
+		
+		
+		session.invalidate();
+		
+		
+		return "redirect:/";
+	}
+}
