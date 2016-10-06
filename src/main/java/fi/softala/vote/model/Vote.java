629473f9c1c69f4589ef0de51de890b946ce336a
Jenny Kalliosto <jenny.kalliosto@myy.haaga-helia.fi>
2016-10-06 10:10:09 +0300

    vote boolean edited - check comment in vote.java for further

--- src/main/java/fi/softala/vote/model/Vote.java
+++ src/main/java/fi/softala/vote/model/Vote.java
@@ -1,5 +1,6 @@
 package fi.softala.vote.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -8,6 +9,9 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.annotations.Type;
 
 @Entity
 @Table(name="vote")
@@ -25,12 +29,17 @@ public class Vote {
 	@JoinColumn(name = "inno_id")
 	private Innovation innovation;
 	
+	@org.hibernate.annotations.Type(type="yes_no")
+	@NotNull
+	@Column(name = "legit")
 	private boolean legit;
+	// db default 'N', if vote is legit -> 'Y'
 
 	public boolean isLegit() {
 		return legit;
 	}
 
+	
 	public void setLegit(boolean legit) {
 		this.legit = legit;
 	}
