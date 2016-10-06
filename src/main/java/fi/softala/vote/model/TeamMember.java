0bc7d1eb76bc42f087b5c5b0005e0690135c5fbb
Jenny Kalliosto <jenny.kalliosto@myy.haaga-helia.fi>
2016-10-06 12:23:48 +0300

    teammember done - getters setters missing due to eclipse *fu* in comment info about validate via java probability

--- src/main/java/fi/softala/vote/model/TeamMember.java
+++ src/main/java/fi/softala/vote/model/TeamMember.java
@@ -1,22 +1,41 @@
 package fi.softala.vote.model;
 
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import javax.persistence.Entity;
+/*
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+*/
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
 
+@Entity
+@Table(name="team_member")
 public class TeamMember {
- /*	@Getter @Setter private long teamId;
-	@Getter @Setter private String firstName;
-	@Getter @Setter private String lastName;
-
 	
-	@ToString (includeFieldNames=true)
-	@Data (staticConstructor="of")
-	class Exem<T> {
-		@SuppressWarnings("unused")
-		private long teamId;
-		@SuppressWarnings("unused")
-		private T x;
-	} */
+	@Size(min = 1, max = 225)
+	private String firstName;
+	
+	@Size(min = 1, max = 225)
+	private String lastName;
+	
+	@ManyToOne
+	@JoinColumn(name = "team_id")
+	private Team team;
+	
+	/*
+	
+	if to validate team members to not vote their own innovation via java...?
+
+	$ desc table team_member;
+	$ alter table team_member add member_id bigint(20) primary key auto_increment;
+	$ alter table team_member drop column member_id;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long memberId;
+
+	 */
 }
