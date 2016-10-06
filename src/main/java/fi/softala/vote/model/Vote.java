fe51a3549db99fe55cf314f561590fa457c0427c
Jenny Kalliosto <jenny.kalliosto@myy.haaga-helia.fi>
2016-10-06 09:52:37 +0300

    vote java class done - boolean entity missing

--- src/main/java/fi/softala/vote/model/Vote.java
+++ src/main/java/fi/softala/vote/model/Vote.java
@@ -1,25 +1,45 @@
 package fi.softala.vote.model;
 
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="vote")
 public class Vote {
- /*
-	@Getter @Setter private long innoId;
-	@Getter @Setter private long voterId;
-	@Getter @Setter private long teamId;
-	@Getter @Setter private boolean legit;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long voteId;
+	
+	@OneToOne
+	@JoinColumn(name = "voter_id")
+	private Voter voter;
+	
+	@ManyToOne
+	@JoinColumn(name = "inno_id")
+	private Innovation innovation;
+	
+	private boolean legit;
 
-
-	@ToString (includeFieldNames=true)
-	@Data (staticConstructor="of")
-	class Exem<T> {
-		@SuppressWarnings("unused")
-		private long innoId;
-		@SuppressWarnings("unused")
-		private T x;
+	public boolean isLegit() {
+		return legit;
 	}
-	*/
+
+	public void setLegit(boolean legit) {
+		this.legit = legit;
+	}
+
+	@Override
+	public String toString() {
+		return "Vote [voteId=" + voteId + ", voter=" + voter + ", innovation="
+				+ innovation + ", legit=" + legit + "]";
+	}
+ 
+	
 }
