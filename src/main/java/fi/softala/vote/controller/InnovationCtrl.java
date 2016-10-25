081f7364cc85a6742aba6040b8340b1695dd1983
Valleyaapo <aapohlaakso@gmail.com>
2016-10-25 11:48:08 +0300

    something

--- src/main/java/fi/softala/vote/controller/InnovationCtrl.java
+++ src/main/java/fi/softala/vote/controller/InnovationCtrl.java
@@ -1,23 +1,16 @@
 package fi.softala.vote.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.inject.Inject;
-import javax.validation.Valid;
 import javax.servlet.http.HttpSession;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import fi.softala.vote.dao.InnoDAOJdbcImpl;
-import fi.softala.vote.dao.InnovationDAO;
 import fi.softala.vote.model.Innovation;
 
 @Controller
@@ -50,21 +43,7 @@ public class InnovationCtrl {
       		model.addAttribute("innovations", innovations);
       		return "innovations";
     	
-    	
-    	/*	for (int i = 0; i < innovation.length; i++) {
-			innovations.add .....
-			}
-    	 */	
-    	
-//    	ArrayList <Innovation> innovations = new ArrayList<>();
-//    	innovations.add(new Innovation(1, "name1", "topic1"));
-//    	innovations.add(new Innovation(1, "name2", "topic2"));
-//    	innovations.add(new Innovation(1, "name3", "topic3"));
-//    	
-//    	model.addAttribute("innovations", innovations);
-    	
-      //  return "innovations";
-    }
+        }
   
 
     // findOne metodin rakentaminen
