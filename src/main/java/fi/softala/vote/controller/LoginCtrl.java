b070602342f86cf3352bd80fadd586a3fa4f2392
Valleyaapo <aapohlaakso@gmail.com>
2016-10-27 11:38:33 +0300

    voter type select input and start of controller parser

--- src/main/java/fi/softala/vote/controller/LoginCtrl.java
+++ src/main/java/fi/softala/vote/controller/LoginCtrl.java
@@ -2,10 +2,12 @@ package fi.softala.vote.controller;
 
 import javax.servlet.http.HttpSession;
 
+//import org.assertj.core.util.Compatibility.System;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class LoginCtrl {
@@ -18,7 +20,21 @@ public class LoginCtrl {
 		}
 		
 		session.setAttribute("voterName", voterName);
+		String str = voterName;
 		
+		boolean retval = str.contains(" ");
+		if (retval) {
+			
+		
+		String[] splitStr = str.split("\\s+");
+		
+		
+		for (int i=0;i < splitStr.length; i++) {
+			System.out.println(splitStr[i]);
+		}
+		} else {
+			System.out.println("no space");
+		}
 		return "redirect:innovation/index";
 	}
 	
