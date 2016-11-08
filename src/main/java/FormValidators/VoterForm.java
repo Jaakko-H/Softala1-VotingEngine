29ca4a471ce4b8fe4238b632ed238331e0c5c11a
Valleyaapo <aapohlaakso@gmail.com>
2016-11-08 11:36:31 +0200

    voterform

---
+++ src/main/java/FormValidators/VoterForm.java
@@ -0,0 +1,29 @@
+package FormValidators;
+
+public class VoterForm {
+
+    public String fName;
+    public String sName;
+    public String vType;
+	public String getfName() {
+		return fName;
+	}
+	public void setfName(String fName) {
+		this.fName = fName;
+	}
+	public String getsName() {
+		return sName;
+	}
+	public void setsName(String sName) {
+		this.sName = sName;
+	}
+	public String getvType() {
+		return vType;
+	}
+	public void setvType(String vType) {
+		this.vType = vType;
+	}
+    
+   
+    
+}
